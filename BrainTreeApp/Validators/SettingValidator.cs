470ab346109fd9f58e9fb866b6b9e72071ce1abb
Jakub Klekota <jakubklekota@gmail.com>
2016-02-22 10:56:11 +0100

    Add settings validator for BrainTreeWrapper

---
+++ BrainTreeApp/Validators/SettingValidator.cs
@@ -0,0 +1,20 @@
+﻿using BrainTree;
+using FluentValidation;
+using System;
+
+namespace BrainTreeWrapper
+{
+    public class SettingValidator : AbstractValidator<AppSettingsPage.SettingsJson>
+    {
+        public SettingValidator()
+        {
+            RuleFor(jk=>jk.Enviroment).NotEmpty();
+
+            RuleFor(jk => jk.MerchantId).NotEmpty();
+
+            RuleFor(jk => jk.PublicKey).NotEmpty();
+
+            RuleFor(jk => jk.PrivateKey).NotEmpty();
+        }
+    }
+}
